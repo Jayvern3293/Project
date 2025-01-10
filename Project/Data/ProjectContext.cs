81c681d3f1fa8e97e9e19ca9cae5dccd6dbb818f
TAN JAYVERN <2301500A@student.tp.edu.sg>
2025-01-10 16:25:30 +0800

    Scaffolded pages for each entity.

---
+++ Project/Data/ProjectContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Project.Domain;
+
+namespace Project.Data
+{
+    public class ProjectContext : DbContext
+    {
+        public ProjectContext (DbContextOptions<ProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Project.Domain.AdoptionHistory> AdoptionHistory { get; set; } = default!;
+        public DbSet<Project.Domain.Appointment> Appointment { get; set; } = default!;
+        public DbSet<Project.Domain.Donation> Donation { get; set; } = default!;
+        public DbSet<Project.Domain.FAQ> FAQ { get; set; } = default!;
+        public DbSet<Project.Domain.Inquiry> Inquiry { get; set; } = default!;
+        public DbSet<Project.Domain.Organization> Organization { get; set; } = default!;
+        public DbSet<Project.Domain.Pet> Pet { get; set; } = default!;
+        public DbSet<Project.Domain.User> User { get; set; } = default!;
+    }
+}
--- Project/Program.cs
+++ Project/Program.cs
@@ -1,6 +1,15 @@
-using Project.Components;
+﻿using Project.Components;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Project.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContextFactory<ProjectContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ProjectContext") ?? throw new InvalidOperationException("Connection string 'ProjectContext' not found.")));
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -14,6 +23,7 @@ if (!app.Environment.IsDevelopment())
 	app.UseExceptionHandler("/Error", createScopeForErrors: true);
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
+    app.UseMigrationsEndPoint();
 }
 
 app.UseHttpsRedirection();
